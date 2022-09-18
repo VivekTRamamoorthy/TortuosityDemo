980dfec68cb9c508ffc26e04c8fa426f49fc312f
Vivek T Ramamoorthy <vivektramamoorthy@gmail.com>
2022-09-18 22:25:27 +0200

    tortuosity but wrong ans

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let y= new Int16Array(1000);
 
 let counter = 0;
 
-
+const tortuosityDisplay = document.getElementById('tortuosity-display');
 function animate(){
     c.clearRect(0,0,canvas.width,canvas.height)
     c.fillStyle = "#999";
@@ -30,6 +30,8 @@ function animate(){
         }
         c.stroke()
         counter++
+
+        tortuosityDisplay.innerText = 'Tortuosity = '+ tortuosity(x,y,counter);
     }else{
         // for (let index = 0; index <= counter; index++) {
         //     x[index]=0;
@@ -40,3 +42,28 @@ function animate(){
     window.requestAnimationFrame(animate)
 }
 animate();
+
+
+function tortuosity(x,y,counter){
+    // x contains the x coordinates of the path, is 1000 ints long, with non-zeros upto counter
+    // y contains the y coordinates of the path, is 1000 ints long, with non-zeros upto counter
+    // counter contains the index up to which x and y contain values
+
+    // tortuosity formula is given by integral v^2 by (int v)^2
+    // v is given by the direction of x2-x1, y2-y1
+    // 
+    let numerator = 0;
+    for (let index = 0; index < counter; index++) {
+        let v = [x[index+1]-x[index], y[index+1]-y[index]];
+
+        numerator+= v[0]**2 + v[1]**2;
+
+    }
+    let denominator = (x[counter-1]-x[0])**2+(y[counter-1]-y[0])**2
+    console.log(denominator);
+    console.log(numerator);
+    return numerator/denominator;
+
+
+}
+
